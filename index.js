78ca4fe009d4bcafdb09c1a557d190b7995928a3
Tirth Patel <tirthpatel7498@gmail.com>
2020-09-06 21:17:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const core = require('@actions/core');
+const CanvasJS = require('canvasjs');
 const wait = require('./wait');
 const readme_maxPoints = 2;
-    const contributions_maxPoints = 1;
-    const conduct_maxPoints = 1;
-    const license_maxPoints = 1;
-    const gitignore_maxPoints = 1;
-    const citations_maxPoints = 1;
-    let total_score = 0;
-    const testFolder = '.';
-    const fs = require('fs');
+const contributions_maxPoints = 1;
+const conduct_maxPoints = 1;
+const license_maxPoints = 1;
+const gitignore_maxPoints = 1;
+const citations_maxPoints = 1;
 
-    fs.readdir(testFolder, (err, files) => {
-      files.forEach(file => {
+
+var total_score = 0;
+const testFolder = '.';
+const fs = require('fs');
+
+
+
+  //most @actions toolkit packages have async methods
+  async function run() {
+    try {
+      fs.readdirSync(testFolder).forEach(file => {
+        
         if(file == 'README.md'){
           total_score = total_score+2;
         }
@@ -27,20 +35,12 @@ const readme_maxPoints = 2;
         if(file == 'CITATION.md'){
           total_score = total_score+1;
         }
-        // console.log(1);
-        // console.log(file);
       });
-    });
-    console.log(`total_score =  ` + total_score);
+      
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+  }
 
-// most @actions toolkit packages have async methods
-// async function run() {
-//   try {
-//     
-    
-//   } catch (error) {
-//     core.setFailed(error.message);
-//   }
-// }
-
-// run();
+  run();
+  console.log(`Score for this repo =  ` + (total_score/7)*100);
