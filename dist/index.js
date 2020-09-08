021daac7ed283ac9868c518003f6c39c345cb122
Dan Goslen <dgoslen@bandwidth.com>
2020-09-07 22:59:11 -0400

    Why is this so hard?

--- dist/index.js
+++ dist/index.js
@@ -6,26 +6,94 @@ require('./sourcemap-register.js');module.exports =
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 const core = __webpack_require__(186);
+const CanvasJS = __webpack_require__(864);
 const wait = __webpack_require__(258);
+const readme_maxPoints = 2;
+const contributions_maxPoints = 1;
+const conduct_maxPoints = 1;
+const license_maxPoints = 1;
+const gitignore_maxPoints = 1;
+const citations_maxPoints = 1;
 
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+var total_score = 0;
+const testFolder = '.';
+const fs = __webpack_require__(747);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+
+  //most @actions toolkit packages have async methods
+  async function run() {
+    try {
+      fs.readdirSync(testFolder).forEach(file => {
+        
+        if(file == 'README.md'){
+          total_score = total_score+2;
+        }
+        if(file == 'CONTRIBUTING.md'){
+          total_score = total_score+1;
+        }
+        if(file == 'CODE-OF-CONDUCT.md'){
+          total_score = total_score+1;
+        }
+        if(file == 'LICENSE'){
+          total_score = total_score+1;
+        }
+        if(file == 'CITATION.md'){
+          total_score = total_score+1;
+        }
+      });
+      
+    } catch (error) {
+      core.setFailed(error.message);
+    }
   }
+
+  run();
+  var score = (total_score/7)*100;
+  console.log(`Score for this repo =  ` + score);
+
+/*
+Grades Range:
+A+ : 95+
+A  : 90-94
+B+ : 85-89
+B  : 80-84
+C+ : 75-79
+C  : 70-74
+D+ : 65-69
+D  : 60-64
+Redo: 0-59
+*/
+
+if(score >= 95){
+  console.log("Grade: A+");
 }
 
-run();
+if(score >=90 && score <= 94){
+  console.log("Grade: A");
+}
+if(score >=85 && score <= 89){
+  console.log("Grade: B+");
+}
+if(score >=80 && score <= 84){
+  console.log("Grade: B");
+}
+if(score >=75 && score <= 79){
+  console.log("Grade: C+");
+}
+if(score >=70 && score <= 74){
+  console.log("Grade: C");
+}
+if(score >=65 && score <= 69){
+  console.log("Grade: D+");
+}
+if(score >=60 && score <= 64){
+  console.log("Grade: D");
+}
+if(score <= 59){
+  console.log("Grade: Redo");
+}
 
 
 /***/ }),
@@ -372,6 +440,22 @@ let wait = function (milliseconds) {
 
 module.exports = wait;
 
+
+/***/ }),
+
+/***/ 864:
+/***/ ((module) => {
+
+module.exports = eval("require")("canvasjs");
+
+
+/***/ }),
+
+/***/ 747:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs");
 
 /***/ }),
 
--- index.test.js
+++ index.test.js
@@ -14,10 +14,3 @@ test('wait 500 ms', async () => {
   var delta = Math.abs(end - start);
   expect(delta).toBeGreaterThanOrEqual(500);
 });
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
-})
