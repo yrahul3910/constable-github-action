29ad91c2caa2fd86f8527994e4b9a1157f2b4432
Varsha Sharma <varsha95.ananth@gmail.com>
2020-10-24 19:04:19 -0400

    Get all repository details

--- src/constable-app/src/App.js
+++ src/constable-app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { request } from "@octokit/request";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <nav className="navbar bg-dark">
+        {/* <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy" /> */}
+          CONSTABLE
+      </nav>
+      <div>
+        <p className="font-weight-bold py-6">Dashboard</p>
+        <p className="font-weight-light py-6">Subtext</p>
+      </div>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Enter GitHub Repository URL" aria-label="Enter GitHub Repository URL" aria-describedby="button-addon2"/>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={getRepoDetails}>Search</button>
+        </div>
+      </div>
     </div>
   );
+}
+
+const getRepoDetails = async function() {
+  console.log("Making request")
+  const response = await request('GET /repos/{owner}/{repo}', {
+    owner: 'varsha5595',
+    repo: 'constable-github-action'
+  })
+  console.log(response)
+
 }
 
 export default App;
